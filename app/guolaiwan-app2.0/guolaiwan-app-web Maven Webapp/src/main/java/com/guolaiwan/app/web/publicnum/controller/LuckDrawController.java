992716f7e939ccc228a8547299eefad28ef62952
Administrator <Administrator@WIN-P28AUBDLE16>
2019-03-15 07:49:32 +0800

    停止发红包

--- app/guolaiwan-app2.0/guolaiwan-app-web Maven Webapp/src/main/java/com/guolaiwan/app/web/publicnum/controller/LuckDrawController.java
+++ app/guolaiwan-app2.0/guolaiwan-app-web Maven Webapp/src/main/java/com/guolaiwan/app/web/publicnum/controller/LuckDrawController.java
@@ -315,7 +315,7 @@ public class LuckDrawController extends WebBaseControll {
 		ex.export(out);
 	}
 	
-	private long amount=2000000;
+	private long amount=0;
     @RequestMapping("/sendRedPacket")
     public ModelAndView sendRedPacket(HttpServletRequest request){
     	ModelAndView mv = new ModelAndView("luckdraw/package");
