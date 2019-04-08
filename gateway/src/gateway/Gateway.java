65869d63b3020a7d3f3fe0971069c617deec88fc
Administrator <Administrator@WIN-P28AUBDLE16>
2019-04-08 08:23:07 +0800

    分流器留用

---
+++ gateway/src/gateway/Gateway.java
@@ -0,0 +1,97 @@
+package gateway;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.Properties;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Gateway extends HttpServlet {
+
+	/**
+		 * Constructor of the object.
+		 */
+	public Gateway() {
+		super();
+	}
+
+	/**
+		 * Destruction of the servlet. <br>
+		 */
+	public void destroy() {
+		super.destroy(); // Just puts "destroy" string in log
+		// Put your code here
+	}
+
+	/**
+		 * The doGet method of the servlet. <br>
+		 *
+		 * This method is called when a form has its tag value method equals to get.
+		 * 
+		 * @param request the request send by the client to the server
+		 * @param response the response send by the server to the client
+		 * @throws ServletException if an error occurred
+		 * @throws IOException if an error occurred
+		 */
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String stream = request.getParameter("stream");
+		InputStream inStream = new FileInputStream(new File(this.getClass().getResource("/").getPath()+"config.properties"));
+		Properties prop = new Properties();  
+		prop.load(inStream);
+		String[] ips=prop.get("iplist").toString().split(",");
+		String[] rates=prop.get("rates").toString().split(",");
+		double d = Math.random();
+		for (int i=0;i<rates.length;i++) {
+			
+			if(d<Double.parseDouble(rates[i])){
+				response.sendRedirect("http://"+ips[i]+":6690/"+stream);
+				System.out.println("mr say:redirect to "+ips[i]);
+				return;
+			}
+		}
+		
+	}
+
+	/**
+		 * The doPost method of the servlet. <br>
+		 *
+		 * This method is called when a form has its tag value method equals to post.
+		 * 
+		 * @param request the request send by the client to the server
+		 * @param response the response send by the server to the client
+		 * @throws ServletException if an error occurred
+		 * @throws IOException if an error occurred
+		 */
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\">");
+		out.println("<HTML>");
+		out.println("  <HEAD><TITLE>A Servlet</TITLE></HEAD>");
+		out.println("  <BODY>");
+		out.print("    This is ");
+		out.print(this.getClass());
+		out.println(", using the POST method");
+		out.println("  </BODY>");
+		out.println("</HTML>");
+		out.flush();
+		out.close();
+	}
+
+	/**
+		 * Initialization of the servlet. <br>
+		 *
+		 * @throws ServletException if an error occurs
+		 */
+	public void init() throws ServletException {
+		// Put your code here
+	}
+
+}
