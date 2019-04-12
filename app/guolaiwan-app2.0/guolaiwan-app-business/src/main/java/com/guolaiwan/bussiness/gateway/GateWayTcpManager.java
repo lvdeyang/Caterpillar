828ea68c20bbdd4cd3d413f47dbfee8db15fc0c6
Administrator <Administrator@WIN-P28AUBDLE16>
2019-04-12 08:13:22 +0800

    完成二维码验单闸机对接代码编写。

--- app/guolaiwan-app2.0/guolaiwan-app-business/src/main/java/com/guolaiwan/bussiness/gateway/GateWayTcpManager.java
+++ app/guolaiwan-app2.0/guolaiwan-app-business/src/main/java/com/guolaiwan/bussiness/gateway/GateWayTcpManager.java
@@ -29,7 +29,9 @@ public class GateWayTcpManager implements TcpCallBack{
 		byte[] devIdByte=ByteTool.subBytes(bytes, 6, 2);
 		byte[] dataByte=ByteTool.subBytes(bytes, 16, bytes.length-16);
 		long cmd=ByteTool.byteArrayToLong(cmdByte);
-		System.out.println(cmd);
+		long devId=ByteTool.byteArrayToLong(devIdByte);
+		String data=new String(dataByte);
+		System.out.println("recv gateway msg-cmd:"+cmd+"*deviceId:"+devId+"*data:"+data);
 		/*const WORD CMD_S1DATA		= 12400;			//串口1数据上传
 		DATA为串口1数据
 		const WORD CMD_S2DATA		= 12401;			//串口2数据上传
@@ -43,12 +45,41 @@ public class GateWayTcpManager implements TcpCallBack{
 		if(cmd==12400){
 			byte[] cmdSend=new byte[28];
 			//const WORD CMD_OPENRLY = 11000;
-			byte[] sHeaderByte=ByteTool.getBytes(0xFEEF);
+			byte[] sHeaderByte=ByteTool.getBytes((short)0xFEEF);
 			cmdSend[0]=sHeaderByte[0];
 			cmdSend[1]=sHeaderByte[1];
-			byte[] sCmdbyte=ByteTool.getBytes(11000);
+			byte[] sCmdbyte=ByteTool.getBytes((short)11000);
 			cmdSend[2]=sCmdbyte[0];
 			cmdSend[3]=sCmdbyte[1];
+			byte[] sDevidbyte=ByteTool.getBytes((short)devId);
+			cmdSend[6]=sDevidbyte[0];
+			cmdSend[7]=sDevidbyte[1];
+			byte[] sLenbyte=ByteTool.getBytes(28);
+			cmdSend[8]=sLenbyte[0];
+			cmdSend[9]=sLenbyte[1];
+			cmdSend[10]=sLenbyte[2];
+			cmdSend[11]=sLenbyte[3];
+			/*struct LLRLY
+			{
+				   int opt;  //=1开继电器，=0连续开继电器
+				   int rly;
+				   int cnt;
+			};
+			Opt=1时rly=继电器，cnt=开继电器时间（单位：10毫秒）
+			Opt=0时rly=继电器，cnt=开继电器次数，（继电器的开、关时间调用CMD_SETRLYTIME设置）*/
+			byte[] optbyte=ByteTool.getBytes(1);
+			byte[] cntbyte=ByteTool.getBytes(2000);
+			//opt
+			cmdSend[16]=optbyte[0];
+			cmdSend[17]=optbyte[1];
+			cmdSend[18]=optbyte[2];
+			cmdSend[19]=optbyte[3];
+			//cnt
+			cmdSend[24]=cntbyte[0];
+			cmdSend[25]=cntbyte[1];
+			cmdSend[26]=cntbyte[2];
+			cmdSend[27]=cntbyte[3];
+			session.write(cmdSend);
 		}
 		
 	}
