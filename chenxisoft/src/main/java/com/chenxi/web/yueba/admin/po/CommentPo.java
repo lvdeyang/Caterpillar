09f324d39c9e0647eb177a73602a90c385f795ca
Administrator <Administrator@windows10.microdone.cn>
2019-06-21 17:14:57 +0800

    评论管理

--- chenxisoft/src/main/java/com/chenxi/web/yueba/admin/po/CommentPo.java
+++ chenxisoft/src/main/java/com/chenxi/web/yueba/admin/po/CommentPo.java
@@ -16,6 +16,28 @@ public class CommentPo extends AbstractBasePO{
 	private String content;
 	private int score;
 	private String answer;
+	private long userId;
+	private String userName;
+	private String userPhoto;
+	
+	public long getUserId() {
+		return userId;
+	}
+	public void setUserId(long userId) {
+		this.userId = userId;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getUserPhoto() {
+		return userPhoto;
+	}
+	public void setUserPhoto(String userPhoto) {
+		this.userPhoto = userPhoto;
+	}
 	public String getContent() {
 		return content;
 	}
