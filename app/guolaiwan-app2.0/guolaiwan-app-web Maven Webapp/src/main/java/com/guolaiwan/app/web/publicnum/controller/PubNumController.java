bc34de48222a96a87e4459ffef69404626d857ad
liuchangwu <2377780500@qq.com>
2019-08-17 17:47:36 +0800

    微官网Bug修改---刘长武

--- app/guolaiwan-app2.0/guolaiwan-app-web Maven Webapp/src/main/java/com/guolaiwan/app/web/publicnum/controller/PubNumController.java
+++ app/guolaiwan-app2.0/guolaiwan-app-web Maven Webapp/src/main/java/com/guolaiwan/app/web/publicnum/controller/PubNumController.java
@@ -2242,10 +2242,9 @@ public class PubNumController extends WebBaseControll {
 					// 更换订单
 					OrderInfoPO newOrder = (OrderInfoPO) orderInfoPO.clone();
 					newOrder.setId(null);
-					newOrder.setPayMoney((newOrder.getPayMoney() / newOrder.getProductNum()) * numReal);
-					newOrder.setPayMode(PayType.WEICHAT);
+					newOrder.setPayMoney((newOrder.getPayMoney() / newOrder.getProductNum()) * numReal);					
 					newOrder.setProductNum(numReal);
-
+					newOrder.setIswallet(true);
 					conn_order.save(newOrder);
 					conn_order.delete(orderInfoPO);
 					payMoney += newOrder.getPayMoney();
