fe42424177fa4991aefc36849dd09ebea78a1f89
Administrator <Administrator@windows10.microdone.cn>
2019-08-21 19:06:33 +0800

    商户导出增加登录名列

--- app/guolaiwan-app2.0/guolaiwan-app-web Maven Webapp/src/main/java/com/guolaiwan/app/web/admin/controller/MerchantController.java
+++ app/guolaiwan-app2.0/guolaiwan-app-web Maven Webapp/src/main/java/com/guolaiwan/app/web/admin/controller/MerchantController.java
@@ -819,7 +819,7 @@ public class MerchantController extends BaseController {
 		}
 		List<MerchantVO> listvo = MerchantVO.getConverter(MerchantVO.class).convert(listpo, MerchantVO.class);
 		String title = "商户" + DateUtil.format(new Date(), "yyyyMMddhhmmss") + ".xls";
-		String[] headers = new String[] { "序号", "商家名称", "商户地址", "联系人", "联系电话", "板块" };
+		String[] headers = new String[] { "序号", "商家名称", "商户地址", "联系人", "联系电话", "板块","登录名" };
 		List<Object[]> dataList = new ArrayList<Object[]>();
 		if (listvo != null) {
 			for (int i = 0; i < listvo.size(); i++) {
@@ -829,7 +829,7 @@ public class MerchantController extends BaseController {
 				obj[3] = listvo.get(i).getShopLinkperson();
 				obj[4] = listvo.get(i).getShopTel();
 				obj[5] = listvo.get(i).getModularName();
-
+				obj[6] = listvo.get(i).getShopLoginName();
 				dataList.add(obj);
 			}
 		}
