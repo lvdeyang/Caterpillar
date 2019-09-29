db2d9bf84457d3dddc908902d1ccfce8c6d8aca5
Administrator <Administrator@windows10.microdone.cn>
2019-09-29 16:54:35 +0800

    报关，报表，人脸识别

---
+++ app/guolaiwan-app2.0/guolaiwan-app-web Maven Webapp/src/main/java/com/guolaiwan/app/customs/MsgTypeEnums.java
@@ -0,0 +1,7 @@
+package com.guolaiwan.app.customs;
+
+public class MsgTypeEnums {
+
+	public static final String ADD_ORDER="CEB303Message";
+	public static final String ADD_ORDER_BACK="CEB304Message";
+}
